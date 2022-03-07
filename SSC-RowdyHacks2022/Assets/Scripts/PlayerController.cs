45417588ec7617575e0eaa56e8e7c905ea297ce5
Saul Carreon <sacarreon18@gmail.com>
2022-03-07 17:24:01 -0600

    Created working projectile

--- SSC-RowdyHacks2022/Assets/Scripts/PlayerController.cs
+++ SSC-RowdyHacks2022/Assets/Scripts/PlayerController.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    Rigidbody rb;
+    
+    public GameObject projectile;
+    public Transform startProjectile;
+    [SerializeField] private float projSpeed = 375f;
+    
     [SerializeField] private float leftRightMovement;
     [SerializeField] private float upDownMovement;
     [SerializeField] private float playerSpeed = 5f;
 
-
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -23,6 +28,15 @@ public class PlayerController : MonoBehaviour
 
         transform.Translate(leftRightMovement * playerSpeed * Time.deltaTime, 0, 0);
         transform.Translate(0, 0, upDownMovement * playerSpeed * Time.deltaTime);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("pew");
+
+            GameObject bullet;
+            bullet = Instantiate(projectile, startProjectile.position, Quaternion.identity);
+            bullet.GetComponent<Rigidbody>().AddForce(transform.forward * projSpeed);
+        }
 
     }
 }
