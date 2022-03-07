fb22d5b45b4ff5ab75038f9154c9f6c06c9ad12c
Saul Carreon <sacarreon18@gmail.com>
2022-03-07 13:38:38 -0600

    created player movement

---
+++ SSC-RowdyHacks2022/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float leftRightMovement;
+    [SerializeField] private float upDownMovement;
+    [SerializeField] private float playerSpeed = 5f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        leftRightMovement = Input.GetAxis("Horizontal");
+        upDownMovement = Input.GetAxis("Vertical");
+
+        transform.Translate(leftRightMovement * playerSpeed * Time.deltaTime, 0, 0);
+        transform.Translate(0, 0, upDownMovement * playerSpeed * Time.deltaTime);
+
+    }
+}
