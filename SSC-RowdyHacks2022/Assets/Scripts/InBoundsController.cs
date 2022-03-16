5f3dbdb29d11edec46f3eebea3c0ae71683eb3d0
Saul Carreon <sacarreon18@gmail.com>
2022-03-15 23:44:43 -0500

    simplified inbounds script
    made it look less cluttered :)

--- SSC-RowdyHacks2022/Assets/Scripts/InBoundsController.cs
+++ SSC-RowdyHacks2022/Assets/Scripts/InBoundsController.cs
@@ -9,6 +9,7 @@ public class InBoundsController : MonoBehaviour
     [SerializeField] private float screenWidth = 8.3f;
     [SerializeField] private float screenHeight = 4.4f;
     [SerializeField] private float playerXPos;
+    [SerializeField] private float playerZPos;
     
     // Start is called before the first frame update
     void Start()
@@ -16,28 +17,28 @@ public class InBoundsController : MonoBehaviour
         mainCam = Camera.main;
         player = GameObject.FindGameObjectWithTag("Player");
         playerXPos = player.transform.position.x;
-
+        playerZPos = player.transform.position.z;
     }
 
     // Update is called once per frame
     void Update()
     {
         //Debug.Log(player.transform.position);
-        if (player.transform.position.x > screenWidth)
+        if (playerXPos > screenWidth)
         {
-            player.transform.position = new Vector3(screenWidth, player.transform.position.y, player.transform.position.z);
+            player.transform.position = new Vector3(screenWidth, player.transform.position.y, playerZPos);
         }
-        if (player.transform.position.z > screenHeight)
+        if (playerZPos > screenHeight)
         {
-            player.transform.position = new Vector3(player.transform.position.x, player.transform.position.y, screenHeight);
+            player.transform.position = new Vector3(playerXPos, player.transform.position.y, screenHeight);
         }
-        if (player.transform.position.x < -screenWidth)
+        if (playerXPos < -screenWidth)
         {
-            player.transform.position = new Vector3(-screenWidth, player.transform.position.y, player.transform.position.z);
+            player.transform.position = new Vector3(-screenWidth, player.transform.position.y, playerZPos);
         }
-        if (player.transform.position.z < -screenHeight)
+        if (playerZPos < -screenHeight)
         {
-            player.transform.position = new Vector3(player.transform.position.x, player.transform.position.y, -screenHeight);
+            player.transform.position = new Vector3(playerXPos, player.transform.position.y, -screenHeight);
         }
     }
 }
