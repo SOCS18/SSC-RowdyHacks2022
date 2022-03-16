869d0817503b910aef170f9dcf2a7d875a59bf35
Saul Carreon <sacarreon18@gmail.com>
2022-03-15 23:59:43 -0500

    working projectile and enemy
    added projectile that can hurt and kill enemies, we did it gamers

--- SSC-RowdyHacks2022/Assets/Scripts/ProjectileController.cs
+++ SSC-RowdyHacks2022/Assets/Scripts/ProjectileController.cs
@@ -2,10 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyController : MonoBehaviour
+public class ProjectileController : MonoBehaviour
 {
-    [SerializeField] private int health = 5;
-    
     // Start is called before the first frame update
     void Start()
     {
@@ -15,15 +13,12 @@ public class EnemyController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (health <= 0)
-            Destroy(gameObject);
+        
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.tag == "Bullet")
-        {
-            health -= 1;
-        }
+        if (other.gameObject.tag == "Enemy")
+            Destroy(gameObject);
     }
 }
