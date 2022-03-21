56a31db7e99583639f6e6c9f4febb8f03c7d253e
Saul Carreon <sacarreon18@gmail.com>
2022-03-20 21:58:28 -0500

    Connected Main Menu and Main Game
    Set up main menu with a working button for the main scene and a quit game menu for the main game, we're almost there gamers

--- SSC-RowdyHacks2022/Assets/Scripts/TimerController.cs
+++ SSC-RowdyHacks2022/Assets/Scripts/TimerController.cs
@@ -6,19 +6,39 @@ using UnityEngine.UI;
 public class TimerController : MonoBehaviour
 {
     [SerializeField] private float timer = 5;
+    [SerializeField] private bool isCountingDown = false;
+    public GameObject endMenu;
     public Text timerText;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        isCountingDown = true;
+        endMenu.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (timer > 0)
-            timer -= Time.deltaTime;
+        if (isCountingDown)
+        {
+            if (timer > 0)
+            {
+                timer -= Time.deltaTime;
+            }
+            else
+            {
+                DisplayEndMenu();
+                isCountingDown = false;
+            }
+        }
+
         timerText.text = "Time: " + (int)timer;
+    }
+
+    void DisplayEndMenu()
+    {
+        Debug.Log("Timer is done");
+        endMenu.SetActive(true);
     }
 }
