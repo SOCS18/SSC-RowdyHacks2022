6ca10a5a714a793ed91b0a4a47036eabd5924fb5
Saul Carreon <sacarreon18@gmail.com>
2022-03-23 13:27:47 -0500

    removed frame restrictor

--- SSC-RowdyHacks2022/Assets/Scripts/GameController.cs
+++ SSC-RowdyHacks2022/Assets/Scripts/GameController.cs
@@ -11,8 +11,7 @@ public class GameController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        // restrict framerate to a set number
-        Application.targetFrameRate = 144;
+
     }
 
     // Update is called once per frame
